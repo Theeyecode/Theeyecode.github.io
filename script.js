baf5cf4a5264e6fc66efc299ef7f59efe5c2678d
Emmanuel Olajubu <emmanuelolajubu90@gmail.com>
2026-02-25 16:29:24 -0500

    Update email template ID in emailjs.send

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.getElementById('contact-form').addEventListener('submit', function(e) {
   btn.textContent = 'Sending...';
   btn.disabled = true;
 
-  emailjs.send("service_dc0h1mq", "template_oge6otw", {
+  emailjs.send("service_dc0h1mq", "template_4r0qxms, {
     name: name,
     email: email,
     message: message,
